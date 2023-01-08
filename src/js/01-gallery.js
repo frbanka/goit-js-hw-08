a012575e93900ee16381d1e29d74808b11b09117
Jacek Wójciak <fr.banka@gmail.com>
2023-01-08 19:05:13 +0100

    lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryIt = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+</div>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', galleryIt);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
