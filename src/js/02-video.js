88ac697fc01a68156c3169c6203e7813be46a646
Jacek Wójciak <fr.banka@gmail.com>
2023-01-08 19:56:16 +0100

    feeback start

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
+  }, 1000)
+);
+
+function setCurrentTime() {
+  player.setCurrentTime(localStorage.getItem(LOCAL_STORAGE_KEY));
+}
+setCurrentTime();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash';
 
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+const inputForm = document.querySelector('.feedback-form');
+
+inputForm.addEventListener('submit', submitForm);
+inputForm.addEventListener('input', throttle(onFormInput, 250));
+
+function onFormInput(e) {
+  let newFeedback = localStorage.getItem(LOCAL_STORAGE_KEY);
+  newFeedback = newFeedback ? JSON.parse(newFeedback) : {};
+  newFeedback[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(newFeedback));
+}
