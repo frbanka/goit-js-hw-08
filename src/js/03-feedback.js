02c1ff0263d672292b46aae330679b94e958dd24
Jacek Wójciak <fr.banka@gmail.com>
2023-01-16 21:12:24 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,7 +12,7 @@ inputForm.addEventListener('submit', submitForm);
 function onFormInput() {
   let email = inputForm.email.value;
   let message = inputForm.message.value;
-  realtimeStorage = { email, message };
+  let realtimeStorage = { email, message };
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(realtimeStorage));
 }
 
